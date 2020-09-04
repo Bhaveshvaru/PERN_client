82fb55a5abbd94f169a3962613510d8e927966be
BhaveshVaru <bhavesh.varu97@gmail.com>
2020-09-04 19:18:20 +0530

    connected to Backend api and integreted ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import InputTodo from './components/InputTodo';
+
 import './App.css';
+import ListTodo from './components/ListTodo';
 
 function App() {
   return (
     <Fragment>
       <div className="container">
-      <InputTodo />
-        </div>
-      
+        <InputTodo />
+      </div>
+      <ListTodo />
     </Fragment>
   );
 }
